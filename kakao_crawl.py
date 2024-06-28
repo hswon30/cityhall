62aacc09adf936d775ccab7ec40c57d31ade141b
Hasun Won <glashuitte@gmail.com>
2024-06-28 16:07:06 +0900

    Minor edits
    Cleaned code and added testing bits for geocoding

--- kakao_crawl.py
+++ kakao_crawl.py
@@ -6,11 +6,14 @@ from pywinauto.application import Application as app
 from pywinauto import clipboard
 from pywinauto import keyboard
 import time
+import os
+import pandas as pd
 #############################################################################################################
 
 path = "C:\\Program Files (x86)\\Kakao\\KakaoTalk\\KakaoTalk.exe"
 tgtwindow = "용인시 공유전동킥보드 불법주차 신고"
 kwrd = "용인시"
+filepath = 'C:\\Users\\user\\OneDrive\\문서\\카카오톡 받은 파일\\'
 
 
 #setting up for kakao app
@@ -20,44 +23,55 @@ kakao = app('uia').connect(path=path)
 # 프로세스들 찾기 finding all available processes
 procs = pywinauto.findwindows.find_elements()
 
-for proc in procs:
-    # print("Current process:", proc.name)
-    # print(proc)
-    if kwrd in proc.name:
-        print(f'keword {kwrd} found')
-        chat = app('win32').connect(title=proc.name)
-        print("chat selected and connected")
-        chat_dlg = chat[tgtwindow]
-        chat_dlg.print_control_identifiers()
-        # print("windows:", chat.windows())
+def save_to_txt():
+    for proc in procs:
+        # print("Current process:", proc.name)
+        # print(proc)
+        if kwrd in proc.name:
+            print(f'keword {kwrd} found')
+            chat = app('win32').connect(title=proc.name)
+            print("chat selected and connected")
+            chat_dlg = chat[tgtwindow]
+            chat_dlg.print_control_identifiers()
+            # print("windows:", chat.windows())
 
-        # 카카오톡 내보내기 기능 사용
-        # To save as txt file invoking kakao chat export function
-        chat_dlg.Edit.click()
-        chat_dlg.type_keys('^s')
-        time.sleep(1)
-        save_dlg = pywinauto.Desktop(backend="win32").window(title_re='.*다른 이름으로 저장*', found_index=0)
+            # 카카오톡 내보내기 기능 사용
+            # To save as txt file invoking kakao chat export function
+            chat_dlg.Edit.click()
+            chat_dlg.type_keys('^s')
+            time.sleep(1)
+            save_dlg = pywinauto.Desktop(backend="win32").window(title_re='.*다른 이름으로 저장*', found_index=0)
 
-        # 파일이름 지정: 24시간에 한번 수집하기 때문에 년월일로 저장
-        # Since we will collect data every 24hrs, use current YYYY-MM-DD format as filename
-        current_time = datetime.datetime.now()
-        txt_filename = str(current_time).split(" ")[0]+"_logs"
-        save_dlg.set_focus()
-        save_dlg.Edit.set_text(txt_filename)
+            # 파일이름 지정: 24시간에 한번 수집하기 때문에 년월일로 저장
+            # Since we will collect data every 24hrs, use current YYYY-MM-DD format as filename
+            current_time = datetime.datetime.now()
+            txt_filename = str(current_time).split(" ")[0]+"_logs"
+            save_dlg.set_focus()
+            save_dlg.Edit.set_text(txt_filename)
 
-        # 엔터 눌러 저장하기
-        # Hitting Enter key to save
-        save_dlg.type_keys('{ENTER}')
-        time.sleep(1)
+            # 엔터 눌러 저장하기
+            # Hitting Enter key to save
+            save_dlg.type_keys('{ENTER}')
+            time.sleep(1)
 
-        # 다음 저장을 위해 확인 화면 꺼주기
-        # To close the confirmation window
-        time.sleep(2)
-        keyboard.send_keys('{ENTER}')
+            # 다음 저장을 위해 확인 화면 꺼주기
+            # To close the confirmation window
+            time.sleep(2)
+            keyboard.send_keys('{ENTER}')
+
+            return txt_filename
 
 
-def add_to_csv():
-    pass
+# 저장된 카카오 채팅 기록 열기
+# Function to open and read saved log file
+def read_txt(filename):
+    with open(filename, 'r', encoding='utf-8') as file:
+        return file.read()
 
-def add_to_spreadsheet():
-    pass
+
+
+# Driver code
+def main():
+    current = save_to_txt()
+    current_filepath = os.path.join(filepath, current)
+
--- testing.py
+++ testing.py
@@ -6,6 +6,12 @@ from pywinauto import keyboard
 import time
 import os
 import re
+from geopy.geocoders import Nominatim
+from geopy.extra.rate_limiter import RateLimiter
+#import googlemaps
+from functools import partial
+import requests
+
 
 filepath = 'C:\\Users\\user\\Downloads\\Telegram Desktop'
 name = '2024-06-26_log.txt'
@@ -24,16 +30,16 @@ def extract_addresses(chat_history):
     address_pattern = re.compile(r'\[.*?\] \[.*?\] - (.*?)\n', re.MULTILINE)
 
     address_patterns = [
-        r'\[.*?\] \[.*?\] 1\.\s*(.*)',  # Matches '1. address'
-        r'\[.*?\] \[.*?\] -\s*(.*)',  # Matches '- address'
+        r'\[.*?\] \[.*?\] 1\.\s*(.*)',
+        r'\[.*?\] \[.*?\] -\s*(.*)',
         r'\[.*?\] \[.*?\] 위반장소 -\s*(.*)',
         r'\[.*?\] \[.*?\] 1\.\위반장소\s*\-\s*(.*)'
-        r'\[.*?\] \[.*?\] -\s*\위반장소\s*\-\s*(.*)'# Matches '위반장소 - address'
+        r'\[.*?\] \[.*?\] -\s*\위반장소\s*\-\s*(.*)'
     ]
 
     date_pattern = re.compile(r'--------------- (\d{4}년 \d{1,2}월 \d{1,2}일 .*?) ---------------')
 
-    dates = date_pattern.findall(chat_history)
+    # dates = date_pattern.findall(chat_history)
 
     addresses = []
     for pattern in address_patterns:
@@ -42,16 +48,43 @@ def extract_addresses(chat_history):
 
     for address in addresses:
         print(address)
+    return addresses
     # date_address_pairs = list(zip(dates, addresses))
     #
     # for pair in date_address_pairs:
     #     print(pair)
 
+# Using google maps API(paid account)
+def geocoding_gmap(address):
+    #import googlemaps
+    gmaps = googlemaps.Client(key='Your_API_Key')
+    geocode_result = gmaps.geocode(address)
+    locs = geocode_result[0]['geometry']['locations'] #{'lat' : x, 'lng' : y}
+    return locs
+
+# Using free geopy(limited functionality)
+def geocoding_geopy(address):
+    # print(address)
+    geolocator = Nominatim(user_agent="South Korea")
+    geocode = RateLimiter(geolocator.geocode, min_delay_seconds=1)
+    loc = geocode(address, language='KOR')
+    if loc:
+        print(f"location {address} found")
+        # print(f'log: {loc.longitude}, lat: {loc.latitude}, addr: {address}')
+        return loc.latitude, loc.longitude
+    else:
+        print(f"unable to code location: {address}")
+    # return loc.longitude, loc.latitude, address
+
+#Using kakao API(rate limited)
+def geocoding_kakao(address):
+    
+
+
 
 chatrec = read_chat_history(fullpath)
-extract_addresses(chatrec)
-
+addresses = extract_addresses(chatrec)
+locex = geocoding_geopy("성복 2로 86")
 
 #https://velog.io/@s0young/Troubleshooting-GeoPy%EC%9D%98-geocoding-%EC%B2%98%EB%A6%AC-%EC%86%8D%EB%8F%84%EB%8A%94-%EC%99%9C-%EB%8A%90%EB%A6%B4%EA%B9%8C
-
-
+# https://m.blog.naver.com/rackhunson/222403071709
